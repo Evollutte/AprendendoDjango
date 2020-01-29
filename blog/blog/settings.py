d7ffe5d71985eb4fea55e2b012c2b534d3323b70
Gerson de Sousa Santos Junior <gersonjunior100@gmail.com>
2020-01-29 18:11:33 -0300

    Adicionando IP e setando debug false

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = '3$h4+lf1q3r-0p11b6ft-h6@z@(7r@-!k8*#^^xwcmgm(#dit!'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '34.70.112.10']
+ALLOWED_HOSTS = ['localhost', '35.198.55.179']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
