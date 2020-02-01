49154b5f012015003a43bf451de1815f45b531b5
Gerson Jr <gersonjunior100@gmail.com>
2020-02-01 20:39:59 -0300

    Teste5

--- blog/website/urls.py
+++ blog/website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
+from .views import hello_blog
 from .views import index, planos, contato, duvidas
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('', planos, name='planos'),
-    path('', contato, name='contato'),
-    path('', duvidas, name='duvidas'),
+    path('', hello_blog, name='home_blog'),
+    path('post/<int:id>/', index, name='index'),
+    path('post/<int:id>/', planos, name='planos'),
+    path('post/<int:id>/', contato, name='contato'),
+    path('post/<int:id>/', duvidas, name='duvidas'),
 ]
