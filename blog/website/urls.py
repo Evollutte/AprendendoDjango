4a28bd94dfaa03565cdd0b44bb2cd679c1f82333
Gerson Jr <gersonjunior100@gmail.com>
2020-02-01 23:01:49 -0300

    Teste

--- blog/website/urls.py
+++ blog/website/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .views import index
-from .views import planos, contato, duvidas
+from .views import contato, duvidas, planos
 
 urlpatterns = [
     path('', index, name='home_blog'),
+    path('post/<int:id>/', contato, name='contato'),
+    path('post/<int:id>/', duvidas, name='duvidas'),
     path('post/<int:id>/', planos, name='planos'),
-    
 ]
--- blog/website/views.py
+++ blog/website/views.py
@@ -17,15 +17,14 @@ def index(request):
 
     return render(request, 'index.html', data)
 
+def contato(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'contato.html', {'post': post})
+
+def duvidas(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'duvidas.html', {'post': post})
+
 def planos(request, id):
     post = Post.objects.get(id=id)
     return render(request, 'planos.html', {'post': post})
-
-def save_form(request):
-    name = request.POST['name']
-    Contact.objects.create(
-        name=name, 
-        email=request.POST['email'],
-        message=request.POST['message']    
-    )
-    return render(request, 'contact_success.html', {'name_contact': name})
